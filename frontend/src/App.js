7490123754fd40ba3f1d2030ac7eae8ab28262c5
mehul <mehulraj925@gmail.com>
2025-05-02 16:09:13 -0400

    Updating routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Homepage from './pages/Homepage';
 import Login from './components/auth/login/login';
 import Signup from './components/auth/signup/signup';
 import About from './pages/about';
+import Settings from './pages/settings';
 
 import AlgorithmsPage from './components/algorithm/algorithms';
 import ArraysPage from './pages/arrays'; // Import new ArraysPage component
@@ -41,6 +42,7 @@ const App = () => {
                         <Route path="/login" element={<Login />} />
                         <Route path="/signup" element={<Signup />} />
                         <Route path="/about" element={<About />} /> {/* Add the route for About page */}
+                        <Route path="/settings" element={<Settings />} />
                         <Route path="/graphs" element={<GraphVisualization />} />
                         <Route path="/trees" element={<TreeVisualization />} />
                         <Route path="/sorting" element={<SortingAlgorithms />} />
